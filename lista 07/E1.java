d82ae4d90f7744712ea4292dbad894efe31a0d17
Lorrany Boschesi <126982697+boschesi1@users.noreply.github.com>
2023-05-16 14:45:09 -0300

    Add files via upload

---
+++ lista 07/E1.java
@@ -0,0 +1,37 @@
+package lista7;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class E1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		Boolean sair = true;
+		do {
+			try {
+				
+					System.out.println("Digite o nome do arquivo: ");
+					String arquivo = sc.nextLine();				
+					FileInputStream in = new FileInputStream(arquivo);
+					sair = false;
+					int c = 0;
+					while( ( c = in.read() ) != -1 )
+						System.out.println(c);
+				
+			} 
+			catch (FileNotFoundException e) {
+				System.out.println("Arquivo não encontrado !!");
+			} 
+			catch (IOException e) {
+				System.out.println("Falha na leitura do arquivo !!");
+			}
+		}
+		while( sair );
+	}
+
+}
+  //Exercício 1
